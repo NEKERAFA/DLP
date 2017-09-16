1ec4fd663f20c1d1c1019d1ca8cdb6a0ca4948c5
EvaSuarezGarcia <eva.suarez.garci@gmail.com>
2017-09-16 13:39:05 +0200

    Añadida clase Node

--- Java/bst/src/bst/Bst.java
+++ Java/bst/src/bst/Bst.java
@@ -1,10 +1,21 @@
+/* 
+ * Rafael Alcalde Azpiazu: rafael.alcalde.azpiazu (rafael.alcalde.azpiazu@udc.es)
+ * Eva Suárez García: eva.suarez.garcia (eva.suarez.garcia@udc.es)
+ */
+
 package bst;
 
 public class Bst {
-
-	private Integer key;
-	private Bst leftChild;
-	private Bst rightChild;
 	
+	private Node root;
 	
+	private void insertI(Integer key) {
+		if (this.root.getKey() == null) { // Empty tree
+			this.key = key;
+		} else {
+			Bst newNode = new Bst();
+			newNode.key = key;
+			
+		}
+	}
 }
