b6155992d5ef7d90804d77e2c0ca192e2aefbbcc
Rafael Alcalde Azpiazu <nekerafa@gmail.com>
2017-09-16 13:39:56 +0200

    Añadidos los métodos que me tocaban

--- Java/bst/src/bst/Bst.java
+++ Java/bst/src/bst/Bst.java
@@ -6,5 +6,33 @@ public class Bst {
 	private Bst leftChild;
 	private Bst rightChild;
 	
+	public Bst getRightChild() {
+		return rightChild;
+	}
 	
+	public boolean isEmptyTree() {
+		return key == null;
+	}
+	
+	private void insertR(Bst tree, Integer key) {
+		// Insert in the root
+		if (this.key == null) {
+			this.key = key;
+		// Insert in left tree child
+		} else if (key < this.key) {
+			if (tree.leftChild == null) {
+				tree.leftChild = new Bst();
+			}
+			
+			insertR(tree.leftChild, key);
+		// Insert in right tree child
+		} else if (key > this.key) {
+			if (tree.rightChild == null) {
+				tree.rightChild = new Bst();
+			}
+			
+			insertR(tree.rightChild, key);
+		}
+		// Ignore duplicate
+	}
 }
