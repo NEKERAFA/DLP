75aa3bad017c191582ae829874854b0acdb7f06a
Rafael Alcalde Azpiazu <nekerafa@gmail.com>
2017-09-22 19:24:09 +0200

    Añadida función insertKeyI en OCaml

--- OCaml/bst_imperativo.ml
+++ OCaml/bst_imperativo.ml
@@ -20,3 +20,26 @@ let root a = match !a with
 let isEmptyTree a = match !a with
  	Empty -> true
 	| _ -> false;;
+
+let insertKeyI a key =
+	let newNode = ref (Node (key, (ref Empty), (ref Empty))) in
+	(* Insert in empty tree *)
+	if (!a = Empty) then
+		a := !newNode
+	else
+		let parent = ref Empty in
+		let child = ref (!a) in
+		(* Search for the key's right place in the tree *)
+		while (((!child) <> Empty) && ((root child) <> key)) do
+			parent := !child;
+			if (key < (root child)) then
+				child := !(leftChild child)
+			else
+				child := !(rightChild child)
+		done;
+
+		if (!child = Empty) then
+			if (key < (root parent)) then
+				(leftChild parent) := !newNode
+			else
+				(rightChild parent) := !newNode;;
