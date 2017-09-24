de00b0bff5367e96e5299dae7a8474e6d47694e3
Rafael Alcalde Azpiazu <nekerafa@gmail.com>
2017-09-24 13:19:01 +0200

    Creados makefiles para OCaml, Pascal, C y Java

---
+++ Java/codigo_final/bst/Bst.java
@@ -0,0 +1,12 @@
+/* 
+ * Rafael Alcalde Azpiazu: rafael.alcalde.azpiazu (rafael.alcalde.azpiazu@udc.es)
+ * Eva Suárez García: eva.suarez.garcia (eva.suarez.garcia@udc.es)
+ */
+
+package bst;
+
+public class Bst {
+
+	public Node root;
+
+}
